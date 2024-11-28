dd449f5d67e95d2f4c3a4b485c2023725169a108
Anish Shrestha <sthacruz@gmail.com>
2024-11-28 18:44:50 +1100

    Update package require

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     name="totoro",
     version="0.0.1",
     author="Anish Shrestha",
-    python_requires='>=3.11.5',
+    python_requires=">=3.11.5",
     install_requires=[
         "Flask==2.2.2",
+        "werkzeug==2.2.2",
         "PyJWT==2.8.0",
         "requests==2.31.0",
     ],
